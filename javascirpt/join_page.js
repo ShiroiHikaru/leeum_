0c0771383c2416dd82c29c82d97790191f5eb3b8
ShiroiHikaru <asd95121@naver.com>
2021-10-31 20:18:20 +0900

    git add .

--- javascirpt/join_page.js
+++ javascirpt/join_page.js
@@ -5,4 +5,10 @@ function join(){
   if(user.value == ""){
     alert("이름을 입력해주세요.");
   };
+  
+  var agree = document.getElementById("agree");
+  if(!agree){
+    alert("동의버튼을 클릭하셔야 다음 페이지로 넘어갑니다.");
+    return false;
+  };
 };
