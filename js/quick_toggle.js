ba32bf1fc1147eab1ddeb3440eae462d087136b8
ShiroiHikaru <asd95121@naver.com>
2021-11-05 16:08:46 +0900

    git add .

--- js/quick_toggle.js
+++ js/quick_toggle.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
   $(".b01").click(function(){
     $(".quickform").toggle(500);
   });
+
+  $(".box1 h2").mouseenter(function(){
+    $(this).animate({width:290, height: 400, left:-20, top: 0}, 0.5);
+  });
+
+  $(".box1 h2").mouseleave(function(){
+    $(this).animate({width:290, height: 400, left:0, top: 0});
+  });
+
 });
