d0e635b9d94492abd7cb7080e4dd318c1e53e0d2
ShiroiHikaru <asd95121@naver.com>
2021-11-17 10:42:37 +0900

    git add .

--- js/quick_toggle.js
+++ js/quick_toggle.js
@@ -8,4 +8,10 @@ $(document).ready(function(){
   $(".sitelink h3").click(function(){
     $(".site_list").slideToggle("slow");
   });
+
+  $(".arttext1").hide(function(){
+    $(this).mouseenter(function(){
+      $(this).slideToggle("slow");
+    });
+  });
 });
