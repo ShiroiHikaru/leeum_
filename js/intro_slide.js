0e3a68e1a5c6a33876e17671d235bade9ee0d8b8
ShiroiHikaru <asd95121@naver.com>
2021-12-12 23:53:25 +0900

    git add .

--- js/intro_slide.js
+++ js/intro_slide.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $(".mario .arttext1").hover(function(){
-    $(this).stop(true).fadeToggle("slow");
+    $(this).stop(true).fadeToggle("fast");
+  });
+  $(".jean .arttext2").hover(function(){
+    $(this).stop(true).fadeToggle("fast");
+  });
+  $(".rem .arttext3").hover(function(){
+    $(this).stop(true).fadeToggle("fast");
   });
 });
