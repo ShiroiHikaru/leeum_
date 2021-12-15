4599b80dd4e2f3c69a697910611b0329842c4704
ShiroiHikaru <asd95121@naver.com>
2021-12-15 11:32:29 +0900

    git add .

--- js/join_page.js
+++ js/join_page.js
@@ -1,14 +1,7 @@
-function join(){
-  //변수설정
-  var user = document.getElementById("user");
-  
-  if(user.value == ""){
-    alert("이름을 입력해주세요.");
-  };
-  
-  var agree = document.getElementById("agree");
-  if(!agree){
-    alert("동의버튼을 클릭하셔야 다음 페이지로 넘어갑니다.");
-    return false;
-  };
-};
+function leeum_welcome(){
+  let user = document.getElementById("user");
+  let uid = document.getElementById("uid");
+  let pwd = document.getElementById("pwd");
+  let repwd = document.getElementById("repwd");
+  let front
+}
