172e0bb35a3fee8b3bb1b519984b26de10783c26
ShiroiHikaru <asd95121@naver.com>
2021-12-24 01:27:00 +0900

    git add .

---
+++ js/duplication.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    $(document).ready(function(){
+      $("header").load("header.html");
+      $("#quick").load("quick.html");
+      $("footer").load("footer.html");
+    })
+  </script>
