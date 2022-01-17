8ad7dffffa79118be383634c44f2cc95f7a75b95
ShiroiHikaru <asd95121@naver.com>
2022-01-18 01:36:29 +0900

    faq

--- js/research.js
+++ js/research.js
@@ -15,4 +15,29 @@ $(document).ready(function(){
   $(".qna4 h3").click(function(){
     $(".qna4 p").slideToggle("fast");
   });
+
+  $(".qna5 h3").click(function(){
+    $(".qna5 p").slideToggle("fast");
+  });
+
+  $(".qna6 h3").click(function(){
+    $(".qna6 p").slideToggle("fast");
+  });
+
+  $(".qna7 h3").click(function(){
+    $(".qna7 p").slideToggle("fast");
+  });
+
+  $(".qna8 h3").click(function(){
+    $(".qna8 p").slideToggle("fast");
+  });
+
+  $(".qna9 h3").click(function(){
+    $(".qna9 p").slideToggle("fast");
+  });
+
+  $(".qna10 h3").click(function(){
+    $(".qna10 p").slideToggle("fast");
+  });
+ 
 });
