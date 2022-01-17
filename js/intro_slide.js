6b4a0d839f464ea2eea54780334712559d588ee4
ShiroiHikaru <asd95121@naver.com>
2022-01-17 19:16:08 +0900

    toggle edit

--- js/intro_slide.js
+++ js/intro_slide.js
@@ -3,8 +3,24 @@ $(document).ready(function(){
     $("#interview .artist .artistbox .mario .arttext1").stop(true).slideDown("slow");
   });
 
-  $("#interview .artist .artistbox .mario h2").mouseout(function(){
+  $("#interview .artist .artistbox .mario h2").mouseleave(function(){
     $("#interview .artist .artistbox .mario .arttext1").slideUp("slow");
+  });
+
+  $("#interview .artist .artistbox .jean h2").mouseenter(function(){
+    $("#interview .artist .artistbox .jean .arttext2").stop(true).slideDown("slow");
+  });
+
+  $("#interview .artist .artistbox .jean h2").mouseleave(function(){
+    $("#interview .artist .artistbox .jean .arttext2").slideUp("slow");
+  });
+
+  $("#interview .artist .artistbox .rem h2").mouseenter(function(){
+    $("#interview .artist .artistbox .rem .arttext3").stop(true).slideDown("slow");
+  });
+
+  $("#interview .artist .artistbox .rem h2").mouseleave(function(){
+    $("#interview .artist .artistbox .rem .arttext3").slideUp("slow");
   });
 
   
